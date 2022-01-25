f0296e3762fdf1ff6946678e9d600b0e692dea16
Ernie <emetcalf44@gmail.com>
2022-01-25 21:25:47 +0000

    completed javascript

--- pod-landing-page/script.js
+++ pod-landing-page/script.js
@@ -1,6 +1,34 @@
+const form = document.querySelector('#form')
 const errorMsg = document.querySelector('#error-msg');
-const email = document.querySelector('email');
+const email = document.querySelector('#email');
 
-const displayError = () => {
-    
+// message functions
+const successful = () => {
+    errorMsg.style.color = "#b3ffe2"
+    errorMsg.innerHTML = "Email registered";
+    form.reset();
+}
+
+const unsuccessful = () => {
+    errorMsg.style.color = "red"
+    errorMsg.innerHTML = "Oops! Please check your email"
+}
+
+// email validation
+const checkEmail = (input) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        successful()
+    } else {
+        unsuccessful()
+    }
+
 } 
+
+// event listener
+
+addEventListener ("submit", function(e) {
+    e.preventDefault();
+
+    checkEmail(email)
+})
